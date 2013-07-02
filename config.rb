2162a475aec1f751fdd604a99c8caa8c741bdae5
Brian Graves <git@brianleegraves.com>
2013-07-01 20:22:37 -0500

    activated css & js minify and relative assets

--- config.rb
+++ config.rb
@@ -48,24 +48,15 @@
 # end
 
 set :css_dir, 'stylesheets'
-
 set :js_dir, 'javascripts'
-
 set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
+  activate :minify_css
+  activate :minify_javascript
   # activate :cache_buster
-
-  # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Compress PNGs after build
   # First: gem install middleman-smusher
